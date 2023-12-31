c11559bd6dd2f98e6548b51c9ced22479d89fb96
Zakhar <zakhar@fishandfly.ru>
2023-12-31 22:58:22 +0300

    Registration handler added

--- source/main.py
+++ source/main.py
@@ -1,20 +1,33 @@
 import berserk
 from config import API_TOKEN, player, BOT_TOKEN
 from games_import import download_last_game
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram.ext import ApplicationBuilder, Updater, CommandHandler, MessageHandler, ContextTypes, filters
 from telegram import Update
-from database import init_database
+from player import start_handler
 
 session = berserk.TokenSession(API_TOKEN)
 client = berserk.Client(session)
+# dispatcher = updater.D
+
+
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello, {update.effective_user.first_name}!')
+    await update.message.reply_text(f'Hello, {update.effective_user.first_name}! '
+                                    f'I am smart bot, who make your game style analysys. '
+                                    f'Do /register to register your account.')
+
+async def init_registration(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Пожалуйста, введите свой никнейм lichess-аккаунта:')
+
+    # register_player()
 
 if __name__ == '__main__':
     download_last_game(player, client)
-    init_database()
 
     app = ApplicationBuilder().token(BOT_TOKEN ).build()
+    app.add_handler(start_handler)
     app.add_handler(MessageHandler(filters.TEXT, hello))
+
+
     app.run_polling()
+
 
--- source/player.py
+++ source/player.py
@@ -1,5 +1,47 @@
-def register_player():
+from telegram import Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ConversationHandler,
+    CallbackContext
+)
+
+async def register_player(player_name: str):
     """Регистрирует текущего игрока в системе."""
     pass
 
+# Определяем состояния разговора
+INPUT_TEXT = 0
+
+async def start_register(update: Update, context: CallbackContext) -> int:
+    # Пользователю отправляется приглашение к вводу
+    await update.message.reply_text('Please, enter your lichess account nickname: ')
+    # Переходим в состояние INPUT_TEXT
+    return INPUT_TEXT
+
+async def input_text(update: Update, context: CallbackContext) -> int:
+    # Чтение текста, введенного пользователем
+    user_text = update.message.text
+    await update.message.reply_text(f'You entered: {user_text}')
+    return ConversationHandler.END
+
+async def cancel(update: Update, context: CallbackContext) -> int:
+    # Пользователь отменил ввод
+    await update.message.reply_text('Операция отменена.')
+    # Завершаем разговор
+    return ConversationHandler.END
+
+# Определение точки входа в разговор
+start_handler = CommandHandler('register', start_register)
+
+# Определение обработчика для каждого состояния разговора
+conversation_handler = ConversationHandler(
+    entry_points=[start_handler],
+    states={
+        INPUT_TEXT: [MessageHandler(filters.TEXT & ~filters.COMMAND, input_text)],
+    },
+    fallbacks=[CommandHandler('cancel', cancel)]
+)
 
